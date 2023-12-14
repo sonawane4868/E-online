1f8bd10c5b60ee052d953f6ba764fdd9fa253784
sonawane4868 <111578023+sonawane4868@users.noreply.github.com>
2023-12-14 16:42:51 +0530

    Update content1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Maincomp from './components/Maincomp/Maincomp';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Maincomp />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/" element={<Maincomp />} />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
+    <Maincomp/>
   );
 }
 
